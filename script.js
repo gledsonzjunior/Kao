871d531d2db0a5a310d61492d5b722637e560e6c
047juniorr <gledson.enterprise@gmail.com>
2025-01-14 17:36:10 -0300

    deploy kao

---
+++ script.js
@@ -0,0 +1,45 @@
+const controls = document.querySelectorAll(".control");
+
+let currentItem = 0;
+
+const items = document.querySelectorAll(".item");
+
+const maxItems = items.length;
+
+// biome-ignore lint/complexity/noForEach: <explanation>
+controls.forEach((control) => {
+	control.addEventListener("click", () => {
+		const isLeft = control.classList.contains("arrow-left");
+
+		if (isLeft) {
+			currentItem -= 1;
+		} else {
+			currentItem += 1;
+		}
+
+		if (currentItem < 0) {
+			currentItem = maxItems - 1;
+		} else if (currentItem > maxItems - 1) {
+			currentItem = 0;
+		}
+
+		// biome-ignore lint/complexity/noForEach: <explanation>
+		items.forEach((item) => item.classList.remove("current-item"));
+
+		items[currentItem].scrollIntoView({
+			inline: "center",
+			behavior: "smooth",
+		});
+
+		items[currentItem].classList.add("current-item");
+
+		console.log("control", isLeft, currentItem, maxItems);
+	});
+});
+
+// trocar cor do header
+
+window.addEventListener("scroll", () => {
+	const header = document.querySelector(".header");
+	header.classList.toggle("rolagem", window.scrollY > 0);
+});
